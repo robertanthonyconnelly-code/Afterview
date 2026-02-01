09f718365a262b39bad9853ca381115ac4e12a5e
robertanthonyconnelly-code <robert.anthony.connelly@gmail.com>
2026-02-01 16:43:17 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,20 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Afterview",
+  description: "From Rejection to Real Direction",
+  icons: {
+    icon: "/favicon.ico"
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="wash" />
+        {children}
+      </body>
+    </html>
+  );
+}
